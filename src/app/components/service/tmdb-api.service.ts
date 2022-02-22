fa62e5fcf130beb7dccbb759a5e8e1fa552e3710
gregorylira <gregory.lira007@gmail.com>
2022-02-22 16:21:22 -0300

    adicionando variaveis de ambients

--- src/app/components/service/tmdb-api.service.ts
+++ src/app/components/service/tmdb-api.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Result, RootObject } from 'src/app/model/Filmes';
 import { map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -10,13 +11,14 @@ import { map, Observable } from 'rxjs';
 export class TmdbApiService {
   filmes$?: Observable<Result[]>;
   root$?: Observable<RootObject>;
-  base_URL = 'https://api.themoviedb.org/3/';
+  private readonly base_URL = 'https://api.themoviedb.org/3/';
 
   constructor(private http: HttpClient) {}
 
   getPopulares(): void {
     this.root$ = this.http.get<RootObject>(
-      this.base_URL + `movie/popular?api_key=&language=en-US&page=1`
+      this.base_URL +
+        `movie/popular?api_key=${environment.API_KEY}&language=en-US&page=1`
     );
     this.filmes$ = this.root$.pipe(map((root) => root.results));
   }
